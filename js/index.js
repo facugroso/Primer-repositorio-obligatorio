e26223c1182bbc92a8279ce0b314c5a3c1421658
facugroso <110434971+facugroso@users.noreply.github.com>
2022-09-14 22:26:11 -0300

    eligiendo opciones

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguet").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
