999a21bb0cf92ca6897a68bac6e25a028ed93faf
facugroso <110434971+facugroso@users.noreply.github.com>
2022-10-12 22:38:46 -0300

    ...

---
+++ js/cart.js
@@ -0,0 +1,8 @@
+function showProduCart(array){
+    array.forEach((produ) => {
+        document.getElementById("container").innerHTML += `
+        <a href="cart.html" style="text-decoration:none;">
+        `
+})
+}
+
