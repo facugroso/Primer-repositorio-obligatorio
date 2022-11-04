b3657527a4172147ba32b9c4d48bca836726f2a3
facugroso <110434971+facugroso@users.noreply.github.com>
2022-11-04 20:28:23 -0300

    adelantando

---
+++ js/my-profile.js
@@ -0,0 +1,4 @@
+function imgProfile(img){
+    const imgProfile = document.getElementById("imgPerfil") //voy a tomar el valor de la imagen y lo voy a almacenar en el segundo div del titulo
+    imgProfile.innerHTML = img;
+}
